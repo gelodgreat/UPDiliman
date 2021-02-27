497666eee25faa008d09d73aa015d4d456a095ba
Angelo Umali <carions46@gmail.com>
2021-02-27 16:55:53 +0800

    Implemented react native config

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ApplicationProvider } from '@ui-kitten/components';
 import AuthStack from './routes';
 import * as eva from '@eva-design/eva';
-
+import Config from 'react-native-config';
 import { GoogleSignin } from '@react-native-community/google-signin';
 
 const App = () => {
   useEffect(() => {
     GoogleSignin.configure({
-      webClientId:
-        '3056341095-161sshdpesjmhane7f8gea1ca1ma9v3a.apps.googleusercontent.com',
+      webClientId: Config.WEB_CLIENT_ID,
     });
   }, []);
 
